278f588a36896b7db3159c60c676e03a29282222
Zain-Yousuf <zainyousuf749@gmail.com>
2022-02-13 20:06:52 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <chaild v-bind:users="users"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import chaild from './components/chaild.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    chaild
+  },
+  data(){
+    return {users:[
+      {name:"Huni"},
+      {name:"zain"},
+      {name:"sufyan"},
+      {name:"numan"}
+      ]}
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/chaild.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+<h1>My name is Zain</h1>
+<!-- <h1>{{name}}</h1> -->
+<h3 v-for="user in users" :key="user.name">{{user.name}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'chaild',
+        props:['users']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
